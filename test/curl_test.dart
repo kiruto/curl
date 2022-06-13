34175e75f2b11c43094ffa7911a8ee056df35d72
Klemen Tusar <k.tusar@cmcmarkets.com>
2022-06-13 17:13:00 +0100

    :white_check_mark: Clean up tests

--- test/curl_test.dart
+++ test/curl_test.dart
@@ -6,71 +6,69 @@ import 'package:test/test.dart';
 void main() {
   final Faker faker = new Faker();
 
-  group('Send requests, ', () {
-    late Uri endpoint;
+  late Uri endpoint;
 
-    setUp(() {
-      endpoint = Uri.parse(faker.internet.httpsUrl());
-    });
+  setUp(() {
+    endpoint = Uri.parse(faker.internet.httpsUrl());
+  });
 
-    test("A GET request", () {
-      final http.Request req = http.Request("GET", endpoint);
-      expect(
-        toCurl(req),
-        equals("curl '$endpoint' --compressed --insecure"),
-      );
-    });
+  test("GET request", () {
+    final http.Request req = http.Request("GET", endpoint);
+    expect(
+      toCurl(req),
+      equals("curl '$endpoint' --compressed --insecure"),
+    );
+  });
 
-    test("A POST request, ", () {
-      final http.Request req = http.Request("POST", endpoint);
-      expect(
-        toCurl(req),
-        equals("curl '$endpoint' -X POST --compressed --insecure"),
-      );
-    });
+  test("POST request, ", () {
+    final http.Request req = http.Request("POST", endpoint);
+    expect(
+      toCurl(req),
+      equals("curl '$endpoint' -X POST --compressed --insecure"),
+    );
+  });
 
-    test("A GET request with headers, ", () {
-      final http.Request req = http.Request("GET", endpoint);
-      final String cookie =
-          "sessionid=${faker.randomGenerator.string(18)}; csrftoken=${faker.randomGenerator.string(19)};";
-      final String ua = "Thor";
-      req.headers["Cookie"] = cookie;
-      req.headers["User-Agent"] = ua;
-      expect(
-        toCurl(req),
-        equals(
-          "curl '$endpoint' -H 'Cookie: $cookie' -H 'User-Agent: $ua' --compressed --insecure",
-        ),
-      );
-    });
+  test("GET request with headers", () {
+    final http.Request req = http.Request("GET", endpoint);
+    final String cookie =
+        "sessionid=${faker.randomGenerator.string(18)}; csrftoken=${faker.randomGenerator.string(19)};";
+    final String ua = "Thor";
+    req.headers["Cookie"] = cookie;
+    req.headers["User-Agent"] = ua;
+    expect(
+      toCurl(req),
+      equals(
+        "curl '$endpoint' -H 'Cookie: $cookie' -H 'User-Agent: $ua' --compressed --insecure",
+      ),
+    );
+  });
 
-    test("A POST request with parts, ", () {
-      final http.Request req = http.Request("POST", endpoint);
-      final String part1 = "This is the part one of content";
-      final String part2 = "This is the part two of content😅";
-      final String expectQuery =
-          "part1=This%20is%20the%20part%20one%20of%20content&part2=This%20is%20the%20part%20two%20of%20content%F0%9F%98%85";
-      req.bodyFields = {
-        "part1": part1,
-        "part2": part2,
-      };
-      expect(
-        toCurl(req),
-        equals(
-          "curl '$endpoint' -H 'content-type: application/x-www-form-urlencoded; charset=utf-8' --data '$expectQuery' --compressed --insecure",
-        ),
-      );
-    });
+  test("POST request with parts", () {
+    final http.Request req = http.Request("POST", endpoint);
+    final String part1 = "This is the part one of content";
+    final String part2 = "This is the part two of content😅";
+    final String expectQuery =
+        "part1=This%20is%20the%20part%20one%20of%20content&part2=This%20is%20the%20part%20two%20of%20content%F0%9F%98%85";
+    req.bodyFields = {
+      "part1": part1,
+      "part2": part2,
+    };
+    expect(
+      toCurl(req),
+      equals(
+        "curl '$endpoint' -H 'content-type: application/x-www-form-urlencoded; charset=utf-8' --data '$expectQuery' --compressed --insecure",
+      ),
+    );
+  });
 
-    test("A PUT request with body, ", () {
-      final http.Request req = http.Request("PUT", endpoint);
-      req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
-      expect(
-        toCurl(req),
-        equals(
-          "curl '$endpoint' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
-        ),
-      );
-    });
+  test("PUT request with body", () {
+    final http.Request req = http.Request("PUT", endpoint);
+    req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
+    expect(
+      toCurl(req),
+      equals(
+        "curl '$endpoint' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
+      ),
+    );
   });
 }
