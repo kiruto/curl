3eb4dddd5a1e01622f01d6ac05ed7c08a6524084
Klemen Tusar <k.tusar@cmcmarkets.com>
2022-06-13 18:19:17 +0100

    :white_check_mark: Fix Windows tests

--- lib/src/curl_base.dart
+++ lib/src/curl_base.dart
@@ -4,50 +4,50 @@ import 'package:http/http.dart' as http show Request;
 
 final RegExp _r1 = RegExp(r'"');
 final RegExp _r2 = RegExp(r'%');
-final RegExp _r3 = RegExp(r"\\");
-final RegExp _r4 = RegExp(r"[\r\n]+");
+final RegExp _r3 = RegExp(r'\\');
+final RegExp _r4 = RegExp(r'[\r\n]+');
 final RegExp _r5 = RegExp(r"[^\x20-\x7E]|'");
 final RegExp _r7 = RegExp(r"'");
-final RegExp _r8 = RegExp(r"\n");
-final RegExp _r9 = RegExp(r"\r");
-final RegExp _r10 = RegExp(r"[[{}\]]");
-const String _urlencoded = "application/x-www-form-urlencoded";
+final RegExp _r8 = RegExp(r'\n');
+final RegExp _r9 = RegExp(r'\r');
+final RegExp _r10 = RegExp(r'[[{}\]]');
+const String _urlencoded = 'application/x-www-form-urlencoded';
 
 String _uriEncodeMap(Map<String, String> data) => data.keys
     .map(
       (String key) =>
           "${Uri.encodeComponent(key)}=${Uri.encodeComponent(data[key] ?? '')}",
     )
-    .join("&");
+    .join('&');
 
 String _escapeStringWindows(String str) =>
-    "\"" +
+    '\"' +
     str
-        .replaceAll(_r1, "\"\"")
-        .replaceAll(_r2, "\"%\"")
-        .replaceAll(_r3, "\\\\")
-        .replaceAllMapped(_r4, (match) => "\"^${match.group(0)}\"") +
-    "\"";
+        .replaceAll(_r1, '\"\"')
+        .replaceAll(_r2, '\"%\"')
+        .replaceAll(_r3, '\\\\')
+        .replaceAllMapped(_r4, (match) => '\"^${match.group(0)}\"') +
+    '\"';
 
 String _escapeStringPosix(String str) => _r5.hasMatch(str)
     ? "\$\'" +
         str
-            .replaceAll(_r3, "\\\\")
+            .replaceAll(_r3, '\\\\')
             .replaceAll(_r7, "\\\'")
-            .replaceAll(_r8, "\\n")
-            .replaceAll(_r9, "\\r")
+            .replaceAll(_r8, '\\n')
+            .replaceAll(_r9, '\\r')
             .replaceAllMapped(_r5, (Match match) {
           final String x = match.group(0) ?? '';
           assert(x.length == 1);
           final int code = x.codeUnitAt(0);
           if (code < 256) {
             return code < 16
-                ? "\\x0${code.toRadixString(16)}"
-                : "\\x${code.toRadixString(16)}";
+                ? '\\x0${code.toRadixString(16)}'
+                : '\\x${code.toRadixString(16)}';
           }
           final String c = code.toRadixString(16);
 
-          return "\\u" + ("0000$c").substring(c.length, c.length + 4);
+          return '\\u' + ('0000$c').substring(c.length, c.length + 4);
         }) +
         "'"
     : "'$str'";
@@ -56,27 +56,27 @@ String _escapeString(String str) =>
     Platform.isWindows ? _escapeStringWindows(str) : _escapeStringPosix(str);
 
 String toCurl(http.Request req) {
-  final List<String> command = ["curl"];
+  final List<String> command = ['curl'];
   final List<String> ignoredHeaders = [
-    "host",
-    "method",
-    "path",
-    "scheme",
-    "version",
+    'host',
+    'method',
+    'path',
+    'scheme',
+    'version',
   ];
 
   final List<String> data = [];
 
-  String requestMethod = "GET";
+  String requestMethod = 'GET';
 
   command.add(
     _escapeString(
       req.url.queryParameters.isNotEmpty
-          ? "${req.url.origin}${req.url.path}?${_uriEncodeMap(req.url.queryParameters)}"
-          : "${req.url.origin}${req.url.path}",
+          ? '${req.url.origin}${req.url.path}?${_uriEncodeMap(req.url.queryParameters)}'
+          : '${req.url.origin}${req.url.path}',
     ).replaceAllMapped(
       _r10,
-      (match) => "\\${match.group(0)}",
+      (match) => '\\${match.group(0)}',
     ),
   );
 
@@ -84,8 +84,8 @@ String toCurl(http.Request req) {
 
   if (contentType != null && contentType.indexOf(_urlencoded) == 0) {
     ignoredHeaders.add(HttpHeaders.contentLengthHeader);
-    requestMethod = "POST";
-    data.add("--data");
+    requestMethod = 'POST';
+    data.add('--data');
     data.add(
       _escapeString(
         _uriEncodeMap(req.bodyFields),
@@ -93,14 +93,14 @@ String toCurl(http.Request req) {
     );
   } else if (req.body.isNotEmpty) {
     ignoredHeaders.add(HttpHeaders.contentLengthHeader);
-    requestMethod = "POST";
-    data.add("--data-binary");
+    requestMethod = 'POST';
+    data.add('--data-binary');
     data.add(_escapeString(req.body));
   }
 
   if (req.method != requestMethod) {
     command
-      ..add("-X")
+      ..add('-X')
       ..add(req.method);
   }
 
@@ -111,13 +111,13 @@ String toCurl(http.Request req) {
       key: req.headers[key] ?? '',
   }.forEach(
     (String k, String v) => command
-      ..add("-H")
-      ..add(_escapeString("$k: $v")),
+      ..add('-H')
+      ..add(_escapeString('$k: $v')),
   );
 
   return (command
         ..addAll(data)
-        ..add("--compressed")
-        ..add("--insecure"))
-      .join(" ");
+        ..add('--compressed')
+        ..add('--insecure'))
+      .join(' ');
 }
--- test/curl_test.dart
+++ test/curl_test.dart
@@ -30,116 +30,116 @@ void main() {
     );
   });
 
-  test("GET request", () {
-    final http.Request req = http.Request("GET", endpoint);
+  test('GET request', () {
+    final http.Request req = http.Request('GET', endpoint);
     expect(
       toCurl(req),
       "curl '$endpoint' --compressed --insecure",
     );
   });
 
-  test("GET request with query parameters", () {
-    final http.Request req = http.Request("GET", endpointWithQuery);
+  test('GET request with query parameters', () {
+    final http.Request req = http.Request('GET', endpointWithQuery);
     expect(
       toCurl(req),
-      "curl '$endpointWithQuery' --compressed --insecure",
+      'curl \'$endpointWithQuery\' --compressed --insecure',
     );
   });
 
-  test("GET request with headers", () {
-    final http.Request req = http.Request("GET", endpoint);
+  test('GET request with headers', () {
+    final http.Request req = http.Request('GET', endpoint);
     final String cookie =
-        "sessionid=${faker.randomGenerator.string(32).md5hex}; csrftoken=${faker.randomGenerator.string(32).md5hex};";
-    final String ua = "Thor";
-    req.headers["Cookie"] = cookie;
-    req.headers["User-Agent"] = ua;
+        'sessionid=${faker.randomGenerator.string(32).md5hex}; csrftoken=${faker.randomGenerator.string(32).md5hex};';
+    final String ua = 'Thor';
+    req.headers['Cookie'] = cookie;
+    req.headers['User-Agent'] = ua;
     expect(
       toCurl(req),
-      "curl '$endpoint' -H 'Cookie: $cookie' -H 'User-Agent: $ua' --compressed --insecure",
+      'curl \'$endpoint\' -H \'Cookie: $cookie\' -H \'User-Agent: $ua\' --compressed --insecure',
     );
   });
 
-  test("POST request", () {
-    final http.Request req = http.Request("POST", endpoint);
+  test('POST request', () {
+    final http.Request req = http.Request('POST', endpoint);
     expect(
       toCurl(req),
-      "curl '$endpoint' -X POST --compressed --insecure",
+      'curl \'$endpoint\' -X POST --compressed --insecure',
     );
   });
 
-  test("POST request with query parameters ", () {
-    final http.Request req = http.Request("POST", endpointWithQuery);
+  test('POST request with query parameters ', () {
+    final http.Request req = http.Request('POST', endpointWithQuery);
     expect(
       toCurl(req),
-      "curl '$endpointWithQuery' -X POST --compressed --insecure",
+      'curl \'$endpointWithQuery\' -X POST --compressed --insecure',
     );
   });
 
-  test("POST request with parts", () {
-    final http.Request req = http.Request("POST", endpoint);
-    final String part1 = "This is the part one of content";
-    final String part2 = "This is the part two of content😅";
+  test('POST request with parts', () {
+    final http.Request req = http.Request('POST', endpoint);
+    final String part1 = 'This is the part one of content';
+    final String part2 = 'This is the part two of content😅';
     final String expectQuery =
-        "part1=This%20is%20the%20part%20one%20of%20content&part2=This%20is%20the%20part%20two%20of%20content%F0%9F%98%85";
+        'part1=This%20is%20the%20part%20one%20of%20content&part2=This%20is%20the%20part%20two%20of%20content%F0%9F%98%85';
     req.bodyFields = {
-      "part1": part1,
-      "part2": part2,
+      'part1': part1,
+      'part2': part2,
     };
     expect(
       toCurl(req),
-      "curl '$endpoint' -H 'content-type: application/x-www-form-urlencoded; charset=utf-8' --data '$expectQuery' --compressed --insecure",
+      'curl \'$endpoint\' -H \'content-type: application/x-www-form-urlencoded; charset=utf-8\' --data \'$expectQuery\' --compressed --insecure',
     );
   });
 
-  test("PUT request with body", () {
-    final http.Request req = http.Request("PUT", endpoint);
-    req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
+  test('PUT request with body', () {
+    final http.Request req = http.Request('PUT', endpoint);
+    req.body = 'This is the text of body😅, \\, \\\\, \\\\\\';
     expect(
       toCurl(req),
-      "curl '$endpoint' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
+      '''curl '$endpoint' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure''',
     );
   });
 
-  test("PUT request with body and query parameters", () {
-    final http.Request req = http.Request("PUT", endpointWithQuery);
-    req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
+  test('PUT request with body and query parameters', () {
+    final http.Request req = http.Request('PUT', endpointWithQuery);
+    req.body = 'This is the text of body😅, \\, \\\\, \\\\\\';
     expect(
       toCurl(req),
-      "curl '$endpointWithQuery' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
+      '''curl '$endpointWithQuery' -X PUT -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure''',
     );
   });
 
-  test("PATCH request with body", () {
-    final http.Request req = http.Request("PATCH", endpoint);
-    req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
+  test('PATCH request with body', () {
+    final http.Request req = http.Request('PATCH', endpoint);
+    req.body = 'This is the text of body😅, \\, \\\\, \\\\\\';
     expect(
       toCurl(req),
-      "curl '$endpoint' -X PATCH -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
+      '''curl '$endpoint' -X PATCH -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure''',
     );
   });
 
-  test("PATCH request with body and query parameters", () {
-    final http.Request req = http.Request("PATCH", endpointWithQuery);
-    req.body = "This is the text of body😅, \\, \\\\, \\\\\\";
+  test('PATCH request with body and query parameters', () {
+    final http.Request req = http.Request('PATCH', endpointWithQuery);
+    req.body = 'This is the text of body😅, \\, \\\\, \\\\\\';
     expect(
       toCurl(req),
-      "curl '$endpointWithQuery' -X PATCH -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure",
+      '''curl '$endpointWithQuery' -X PATCH -H 'content-type: text/plain; charset=utf-8' --data-binary \$'This is the text of body\\ud83d\\ude05, \\\\, \\\\\\\\, \\\\\\\\\\\\' --compressed --insecure''',
     );
   });
 
-  test("DELETE request", () {
-    final http.Request req = http.Request("DELETE", endpoint);
+  test('DELETE request', () {
+    final http.Request req = http.Request('DELETE', endpoint);
     expect(
       toCurl(req),
-      "curl '$endpoint' -X DELETE --compressed --insecure",
+      'curl \'$endpoint\' -X DELETE --compressed --insecure',
     );
   });
 
-  test("DELETE request with query parameters ", () {
-    final http.Request req = http.Request("DELETE", endpointWithQuery);
+  test('DELETE request with query parameters ', () {
+    final http.Request req = http.Request('DELETE', endpointWithQuery);
     expect(
       toCurl(req),
-      "curl '$endpointWithQuery' -X DELETE --compressed --insecure",
+      'curl \'$endpointWithQuery\' -X DELETE --compressed --insecure',
     );
   });
 }
